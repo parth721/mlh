b3214c18557d3cfabdb1f5c58a0032464e409340
parth721 <ghoshparth630@gmail.com>
2023-08-08 17:47:06 +0530

    8th aug updates

---
+++ mlh/frontend/static/javascript/geolocation.js
@@ -0,0 +1,37 @@
+// add eventListener
+document.getElementsByClassName("live-location").addEventListener("click", geoLocation);
+
+//function declaration
+function getLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showCoordinates, handleError);
+    }
+    else{
+        alert("Live location not supported by this browser. Please update this Browser.");
+    }
+}
+
+// function to handle the success call back
+function showCoordinates(position){
+    var lattitude = position.coords.lattitude;
+    var longitude = position.coords.longitude;
+//I don't need to show the output coordinates. Instead I send them to ML model
+}
+
+//function to handle the error callback
+function handleError(error){
+    switch(error.code){
+        case error.PERMISSION_DENIED:
+           alert("request denied");
+           break;
+        case error.POSITION_UNAVAILABLE:
+           alert("not available");
+           break;
+        case error.TIMEOUT:
+           alert("timeout");
+           break;   
+        case error.UNKNOWN_ERROR:
+           alert("unknown error");
+           break;
+    }
+}

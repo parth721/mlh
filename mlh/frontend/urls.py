a4d32ac9584b8e4aa131b7c95abbc1606722e3ca
parth721 <ghoshparth630@gmail.com>
2023-09-03 22:47:43 +0530

    Minute updatation

--- mlh/frontend/urls.py
+++ mlh/frontend/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 #create the url
 urlpatterns = [
-    path("", views.home, name="home_page"),
-    path("user", views.user, name="user_page"),
-    path("partner", views.partner, name="partner_page"),
+    path('', views.home, name="home_page"),
+    path('user/', views.user, name="user_page"),
+    path('partner/', views.partner, name="partner_page"),
 ]

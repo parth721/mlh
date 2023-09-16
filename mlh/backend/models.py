523e820ee7afec21f4c219dc3d51fbf3b934812a
parth721 <ghoshparth630@gmail.com>
2023-09-16 21:38:32 +0530

    work on db & on its connection

--- mlh/backend/models.py
+++ mlh/backend/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=50)
+    user_lon= models.IntegerField()
+    user_lat = models.IntegerField()
+    user_phone = models.IntegerField() #how to prepare the countrycode
+    
+    user_area = models.CharField(max_length=30)
+    user_city = models.CharField(max_length=30)
+    user_state = models.CharField(max_length=30)
+    user_country = models.CharField(max_length=3)
+    
+    
+    
+class Partner(models.Model):
+    partner_name = models.CharField(max_length=50)
+    partner_lon = models.IntegerField()
+    partner_lat = models.IntegerField()
+    partner_phone = models.IntegerField()
+    
+    partner_area = models.CharField(max_length=30)
+    partner_city = models.CharField(max_length=30)
+    partner_state = models.CharField(max_length=30)
+    partner_country = models.CharField(max_length=3)
+    
--- mlh/frontend/views.py
+++ mlh/frontend/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from backend.form import UserForm
 # to send http resonse from views file we added django.http
 from django.http import HttpResponse
 
@@ -11,3 +12,13 @@ def user(request) :
 
 def partner(request) :
     return render(request, "html/partner.html")
+
+def user_form(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success_page')
+    else:
+        form = UserForm()
+    return render(request, 'html/user.html', {'form':form})

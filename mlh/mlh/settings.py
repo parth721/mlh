2e0a9dc95bc0fee26e2195b28e3f0876ec14cb88
parth721 <ghoshparth630@gmail.com>
2023-09-17 09:36:58 +0530

    removed not needed files, create form page

--- mlh/mlh/settings.py
+++ mlh/mlh/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'frontend',
     'backend',
-    'database',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

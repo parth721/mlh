ba06a39ba7447c1cd586a96bd29592b86f4ee9fc
parth721 <ghoshparth630@gmail.com>
2023-10-14 20:42:09 +0530

    changes in views.py

--- mlh/frontend/views.py
+++ mlh/frontend/views.py
@@ -8,7 +8,6 @@ import random
 def home(request) :
     return render(request, "html/home.html")
 
-
 def user_form(request):
     if request.method == 'POST':
         form_user = UserForm(request.POST)
@@ -50,15 +49,21 @@ def redirect_partner(request):
         bio_partner = PartnerRef()
         
     return  render(request, 'html/partnerbio.html', {'bio_partner':bio_partner})
-    
+  
+##### verification code  
     
 def send_verification_code(phone_number):
     code = str(random.randint(1000,9999))
-    
-    client = Client()
-    message = client.messages.create(...)
-    
+    client = Client("AC157a3deced6810930de61fcd331c090d", "22a173d4fad92dedf5ba699ca09fea7e")
+    #client = Client()
+    message = client.messages.create(to=["+91"+phone_number], from_="+15178360990", body=f'Your OTP is : {code}') 
     return code
+
+def resendOTP(self):
+        self.n = random.randint(1000,9999)
+        self.client = Client("AC157a3deced6810930de61fcd331c090d", "22a173d4fad92dedf5ba699ca09fea7e")
+        self.client.messages.create(to=["+91"+self.phone_number], from_="+15178360990", body=f'Your OTP is : {self.n}')
+
 
 def verify_phone_number(request):
     if request.method == "POST":
@@ -67,9 +72,9 @@ def verify_phone_number(request):
         phone_number = request.POST.get('phone_number')
 
         #check existing user or not   
-        user = User.objects.filter(phone_number=phone_number).first()
+        user = User.objects.filter(phone_number = phone_number).first()
         if not user :
-            user = User.objects.create(name= name, country_code=country_code, phone_number=phone_number )
+            user = User.objects.create(name = name, country_code = country_code, phone_number = phone_number )
             
         verification_code = send_verification_code(phone_number)
         
@@ -81,7 +86,8 @@ def verify_phone_number(request):
 def enter_verification_code(request):
     if request.method == "POST":
         entered_code = request.POST.get('code')
-        if entered_code == request.session.get('verification_code'):
-            return render()
+        saved_code = request.session.get('verification_code')
+        if entered_code == saved_code :
+            return render()                      #need some upgrade
         else:
-            
+            return redirect(request, 'html/login.html')       #need some upgrades     

2a04a5835feb7ed52c713206b0ce9a76768ad584
parth721 <ghoshparth630@gmail.com>
2023-10-24 23:24:28 +0530

    developing the model 90% done

--- mlh/backend/form.py
+++ mlh/backend/form.py
@@ -3,20 +3,21 @@ from .models import *
 
 class UserForm(forms.ModelForm):
     class Meta :
-        model = User
-        fields = ['user_name', 'user_country', 'user_phone']
+        model = UserInfo
+        fields = ['Name', 'Phone', 'Country']
 
 class UserRef(forms.ModelForm):
     class Meta:
         model = UserBio
-        fields = ['user_purpose','user_area', 'user_city', 'user_state', 'user_lat', 'user_lon']
+        fields = ['Purpose','Area', 'City', 'State', 'user_lat', 'user_lon']
         
 class PartnerForm(forms.ModelForm):
     class Meta :
-        model = Partner
-        fields = ['partner_name', 'partner_country', 'partner_phone']
+        model = PartnerInfo
+        fields = ['partner_name', 'partner_phone', 'partner_country']
+        
 
 class PartnerRef(forms.ModelForm):
     class Meta:
         model = PartnerBio
-        fields = ['partner_area', 'partner_city', 'partner_state', 'partner_lat', 'partner_lon']        
+        fields = ['partner_purpose', 'partner_area', 'partner_city', 'partner_state', 'partner_lat', 'partner_lon']        

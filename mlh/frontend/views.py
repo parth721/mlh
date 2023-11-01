0b9894783f17f3576c3c6603990294bc5f0acc97
parth721 <ghoshparth630@gmail.com>
2023-11-01 22:05:06 +0530

    using defaulit user model class

--- mlh/frontend/views.py
+++ mlh/frontend/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from backend.form import *
 from .models import *
 from django.http import HttpResponseBadRequest
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login #as auth_login
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 
 '''
@@ -97,7 +99,7 @@ def redirect_partner(request):
 '''
 
 def login(request):
-    return render(request, "html/login.html")
+    return render(request, "/html/login.html")
 '''
 def register_view(request):
     if request.method == 'POST':
@@ -162,11 +164,27 @@ def register(request):
         form = UserForm(request.POST)
         if form.is_valid():
             user = form.save()
-            user.set_password(user.password)
-            user.save()
-            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
             login(request, user)
-            return redirect('user_page')
+            messages.success(request, "Registration successful.")
+            return redirect('login_page')
+        messages.error(request, "Registration unsuccessful.")
     else:
         form = UserForm()
-    return render(request, 'html/register.html', {'form': form})
+    return render(request, 'html/register.html', {'register_form': form})
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None: 
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect('user_page')
+            else:
+                messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()            
+    return render(request, 'html/login.html', {'login_form': form})

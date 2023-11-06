22ca020e350fe0035197572d1d833f653c6fd77b
parth721 <ghoshparth630@gmail.com>
2023-11-06 15:17:26 +0530

    design registration page

--- mlh/backend/views.py
+++ mlh/backend/views.py
@@ -1,14 +1,1 @@
 from django.shortcuts import render
-
-class Geocoding :
-    def Geocoding (self, request):
-       area = request.area
-       city = request.city
-       state = request.state
-       country = request.country
-       # for url-encoded palce use urllib.parse
-       ... 
-
-class Geosearch :
-    # looking for 
-    ...
